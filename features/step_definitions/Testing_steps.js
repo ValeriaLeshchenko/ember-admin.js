cdeaf4769cacb0f69b51218880b3ba56dda4b788
Opak Alex <opak.alexandr@gmail.com>
2014-01-06 14:45:24 +0200

    add first working cucumber scenario

--- features/step_definitions/Testing_steps.js
+++ features/step_definitions/Testing_steps.js
@@ -1,20 +1,42 @@
-function Testing_steps() {
+addStepDefinitions(function (scenario) {
+  // Provide a custom World constructor. It's optional, a default one is supplied.
+  scenario.World = function (callback) {
+    callback();
+  };
+
+
+
+  // Define your World, here is where you can add some custom utlity functions you
+  // want to use with your Cucumber step definitions, this is usually moved out
+  // to its own file that you include in your Karma config
+  var proto = scenario.World.prototype;
+  proto.appSpecificUtilityFunction = function someHelperFunc() {
+    // do some common stuff with your app
+  };
+
+  scenario.Before(function (callback) {
+    // Use a custom utility function
+    this.appSpecificUtilityFunction();
+
+    callback();
+  });
+
   var firstNumber;
   var secondNumber;
   var sum = 0;
 
-  this.Given(/^I have the number (\d+) and (\d+)$/, function(arg1, arg2, callback) {
+  scenario.Given(/^I have the number (\d+) and (\d+)$/, function(arg1, arg2, callback) {
     firstNumber = parseInt(arg1);
     secondNumber = parseInt(arg2);
     callback();
   });
 
-  this.When(/^I add them together$/, function(callback) {
+  scenario.When(/^I add them together$/, function(callback) {
     sum = firstNumber + secondNumber;
     callback();
   });
 
-  this.Then(/^I should have (\d+)$/, function(arg1, callback) {
+  scenario.Then(/^I should have (\d+)$/, function(arg1, callback) {
     var expectedSum = parseInt(arg1);
     if (expectedSum !== sum) {
       throw new Error('It doesn\'t add up! ' + arg1 + ' !== ' + sum);
@@ -22,6 +44,7 @@ function Testing_steps() {
     callback();
   });
 
-};
-
-module.exports = Testing_steps;
+  scenario.After(function (callback) {
+    callback();
+  });
+});
